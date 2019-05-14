1a5837b1221cb34e1f81103a810f27211f8a96ec
Amir Bawab <amirbawab@gmail.com>
2019-05-13 22:47:42 -0400

    Added watwriter example

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,17 @@
 #include <src/ir.h>
+#include <src/wat-writer.h>
+#include <src/stream.h>
+#include <src/cast.h>
 
 int main() {
   wabt::ExprList list;
   list.push_back(wabt::MakeUnique<wabt::NopExpr>());
+  wabt::Module module;
+  auto field = wabt::MakeUnique<wabt::FuncModuleField>();
+  module.AppendField(std::move(field));
+  wabt::WriteWatOptions watOptions;
+  auto stream = wabt::FileStream(stdout);
+  wabt::WriteWat(&stream, &module, watOptions);
+
   return 0;
 }
